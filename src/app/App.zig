bae8c4d71bb58ded1962d52b17062c0fb489b86c
Binaries <ethan.ramdeen64@gmail.com>
2026-07-23 20:38:59 -0400

    more progress on the App side of things. next goal is spawning entities in the world, and messing with them

--- src/app/App.zig
+++ src/app/App.zig
@@ -1,13 +1,28 @@
 const std = @import("std");
+const ecs = @import("zflecs");
 
 const App = @This();
 
 alloc: std.mem.Allocator,
+world: *ecs.world_t,
+
+// TODO
+//     plugins.
+//     add plugins
+//     add systems
+//     add components
+//     add tags
+//     ...  
 
 pub fn init(alloc: std.mem.Allocator) App {
     return .{
         .alloc = alloc,
+        .world = ecs.init(),
     };
+}
+
+pub fn deinit(app: *App) void {
+    _ = ecs.fini(app.world);
 }
 
 pub fn run(app: *App) !void {
@@ -15,81 +30,17 @@ pub fn run(app: *App) !void {
     // todo : actually run the app
 }
 
-// old app code from a previous engine project
-// const sapp = @import("sokol").app;
-// const sg = @import("sokol").gfx;
-// const Color = sg.Color;
-// const color = @import("../rendering/color.zig");
-//
-// const Renderer = @import("../rendering/Renderer.zig").Renderer;
-//
-// pub const AppSettings = struct {
-//     width: i32 = 640,
-//     height: i32 = 480,
-//     title: [*c]const u8 = "My App",
-//     clear_color: Color = color.rgb(50, 50, 50),
-// };
-//
-// pub const App = @This();
-//
-// width: i32,
-// height: i32,
-// title: [*c]const u8,
-// renderer: Renderer = .{},
-// // icon: todo
-//
-// const Self = @This();
-//
-// /// global pointer to self, should only be used by functions
-// /// called within `run` as it is null until `run` is called
-// var ptr: ?*Self = null; // I hate this but I gotta do what I gotta do
-//
-// /// initialize a new app
-// pub fn init(settings: AppSettings) App {
-//     return .{
-//         .height = settings.height,
-//         .width = settings.width,
-//         .title = settings.title,
-//         .renderer = .{ .clear_color = settings.clear_color }
-//     };
-// }
-//
-// fn init_cb() callconv(.c) void {
-//     ptr.?.renderer.init();
-// }
-//
-// fn frame_cb() callconv(.c) void {
-//     const self = ptr.?;
-//     const delta = sapp.frameDuration();
-//     self.update(delta);
-//     self.render();
-// }
-//
-// fn cleanup_cb() callconv(.c) void {
-//     ptr.?.renderer.shutdown();
-// }
-//
-// fn update(self: *Self, delta: f64) void {
-//     _ = self;
-//     _ = delta;
-// }
-//
-// fn render(self: *Self) void {
-//     self.renderer.beginFrame();
-//     self.renderer.endFrame();
-// }
-//
-// /// run the app
-// pub fn run(self: *Self) void {
-//     ptr = self;
-//
-//     sapp.run(.{
-//         .init_cb = init_cb,
-//         .frame_cb = frame_cb,
-//         .cleanup_cb = cleanup_cb,
-//         .window_title = self.title,
-//         .width = self.width,
-//         .height = self.height,
-//     });
-// }
+/// add the given components to the ecs
+pub fn addComponents(app: *App, comptime components: anytype) void {
+    inline for (components) |C| ecs.COMPONENT(app.world, C);
+}
 
+/// add the given tags to the ecs
+pub fn addTags(app: *App, comptime tags: anytype) void {
+    inline for (tags) |T| ecs.TAG(app.world, T);
+}
+
+/// add the given systems to the ecs
+pub fn addSystem(app: *App, name: [*:0]const u8, phase: ecs.entity_t, comptime fn_system: anytype) void {
+    _ = ecs.ADD_SYSTEM(app.world, name, phase, fn_system);
+}
--- src/main.zig
+++ src/main.zig
@@ -3,55 +3,46 @@ const Io = std.Io;
 
 const ve = @import("VoxelEngine");
 const ecs = ve.ecs;
+const fVec3 = ve.math.fVec3;
 
-const Position = struct { x: f32, y: f32 };
-const Velocity = struct { x: f32, y: f32 };
+/// NOTE : we have to wrap these in structs otherwise flecs will register them as the same type,
+/// maybe in the future we can have a way for the engine to auto-wrap these to reduce headaches later.
+const Position = struct { vec: fVec3 };
+const Velocity = struct { vec: fVec3 };
 const Eats = struct {};
 const Apples = struct {};
 
 fn move_system(positions: []Position, velocities: []const Velocity) void {
-    for (positions, velocities) |*p, v| {
-        p.x += v.x;
-        p.y += v.y;
-    }
+    for (positions, velocities) |*p, v| p.vec += v.vec;
 }
 
-//Optionally, systems can receive the components iterator (usually not necessary)
+// Optionally, systems can receive the components iterator (usually not necessary)
 fn move_system_with_it(it: *ecs.iter_t, positions: []Position, velocities: []const Velocity) void {
     const type_str = ecs.table_str(it.world, it.table).?;
     std.debug.print("Move entities with [{s}]\n", .{type_str});
     defer ecs.os.free(type_str);
 
-    for (positions, velocities) |*p, v| {
-        p.x += v.x;
-        p.y += v.y;
-    }
+    for (positions, velocities) |*p, v| p.vec += v.vec;
 }
 
 pub fn main(init: std.process.Init) !void {
-    _ = init;
-    const world = ecs.init();
-    defer _ = ecs.fini(world);
+    // const bob = ecs.new_entity(world, "Bob");
+    // _ = ecs.set(world, bob, Position, .{ .vec = fVec3{0, 0, 0} });
+    // _ = ecs.set(world, bob, Velocity, .{ .vec = fVec3{1, 2, 3} });
+    // ecs.add_pair(world, bob, ecs.id(Eats), ecs.id(Apples));
 
-    ecs.COMPONENT(world, Position);
-    ecs.COMPONENT(world, Velocity);
+    // _ = ecs.progress(world, 0);
+    // _ = ecs.progress(world, 0);
 
-    ecs.TAG(world, Eats);
-    ecs.TAG(world, Apples);
-
-    _ = ecs.ADD_SYSTEM(world, "move system", ecs.OnUpdate, move_system);
-    _ = ecs.ADD_SYSTEM(world, "move system with iterator", ecs.OnUpdate, move_system_with_it);
-
-    const bob = ecs.new_entity(world, "Bob");
-    _ = ecs.set(world, bob, Position, .{ .x = 0, .y = 0 });
-    _ = ecs.set(world, bob, Velocity, .{ .x = 1, .y = 2 });
-    ecs.add_pair(world, bob, ecs.id(Eats), ecs.id(Apples));
-
-    _ = ecs.progress(world, 0);
-    _ = ecs.progress(world, 0);
-
-    const p = ecs.get(world, bob, Position).?;
-    std.debug.print("Bob's position is ({d}, {d})\n", .{ p.x, p.y });
-    // var app = ve.App.init(init.arena.allocator());
-    // try app.run();
+    // const p = ecs.get(world, bob, Position).?;
+    // std.debug.print("Bob's position is ({d}, {d}, {d})\n", .{ p.vec[0], p.vec[1], p.vec[2] });
+    var app = ve.App.init(init.arena.allocator());
+    defer app.deinit();
+    app.addComponents(.{ Position, Velocity });
+    app.addTags(.{ Eats, Apples });
+    app.addSystem("move system", ecs.OnUpdate, move_system);
+    app.addSystem("move system with iterator", ecs.OnUpdate, move_system_with_it);
+    // TODO : spawn entities
+    // TODO : progress
+    try app.run();
 }
